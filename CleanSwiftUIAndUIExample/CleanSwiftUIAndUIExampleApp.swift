b9bb24ec1b633e17ef1226c0348a6be01c5641ca
fumiyasac <just1factory@gmail.com>
2023-06-17 02:06:19 +0900

    initial commit

---
+++ CleanSwiftUIAndUIExample/CleanSwiftUIAndUIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanSwiftUIAndUIExampleApp.swift
+//  CleanSwiftUIAndUIExample
+//
+//  Created by 酒井文也 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct CleanSwiftUIAndUIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
