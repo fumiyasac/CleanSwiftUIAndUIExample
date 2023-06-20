8a4b34d5b9d63d4bd7eef192c011d779eddb3e2d
fumiyasac <just1factory@gmail.com>
2023-06-21 00:05:59 +0900

    UI実装サンプル方針を記載

--- CleanSwiftUIAndUIExample/CleanSwiftUIAndUIExampleApp.swift
+++ CleanSwiftUIAndUIExample/CleanSwiftUIAndUIExampleApp.swift
@@ -7,6 +7,12 @@
 
 import SwiftUI
 
+// 「Clean Swift」関連の記事を参考にUI実装を進めていく
+// https://clean-swift.com/clean-swift-ios-architecture/
+// https://nalexn.github.io/clean-architecture-swiftui/
+// Github Repository:
+// https://github.com/nalexn/clean-architecture-swiftui
+
 @main
 struct CleanSwiftUIAndUIExampleApp: App {
     var body: some Scene {
